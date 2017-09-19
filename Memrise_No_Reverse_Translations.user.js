71285cc89a0339fe6a19cfa2ff7bc9a9744fa3b1
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-09-19 13:21:27 +0100

    fix after memrise update

--- Memrise_No_Reverse_Translations.user.js
+++ Memrise_No_Reverse_Translations.user.js
@@ -4,7 +4,7 @@
 // @description    Disables reverse translations when using Memrise
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.1.2
+// @version        0.1.3
 // @updateURL      https://github.com/cooljingle/memrise-no-reverse-translations/raw/master/Memrise_No_Reverse_Translations.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-no-reverse-translations/raw/master/Memrise_No_Reverse_Translations.user.js
 // @grant          none
@@ -15,17 +15,18 @@ $(document).ready(function() {
     MEMRISE.garden.boxes.load = (function() {
         var cached_function = MEMRISE.garden.boxes.load;
         return function() {
+            var result = cached_function.apply(this, arguments);
             enableNoReverseTranslations();
-            return cached_function.apply(this, arguments);
+            return result;
         };
     }());
-    
+
     function enableNoReverseTranslations() {
-        MEMRISE.garden.session.box_factory.make = (function() {
+        MEMRISE.garden.session.box_factory.make = (function () {
             var cached_function = MEMRISE.garden.session.box_factory.make;
-            return function() {         
+            return function () {
                 var result = cached_function.apply(this, arguments);
-                result.forwards = true;
+                result.template = result.template.replace("reversed_", "");
                 return result;
             };
         }());
